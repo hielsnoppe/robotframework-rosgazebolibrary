b138d1ef9b88e744789f834f48d703cbd692e826
David Wedekind <wedekind@hu-berlin.de>
2021-02-09 18:03:25 +0100

    fixed global path

--- RobotRosGazeboLibrary/globals.py
+++ RobotRosGazeboLibrary/globals.py
@@ -1,2 +1,2 @@
 RTF_FILE_PATH = "RTF.log"
-STONE_PATH = "/catkin_ws/src/ros-packages/xivt_fokus_cube/models/stone_10_1_2_5cm"
+STONE_PATH = "/catkin_ws/src/ros-packages/xivt_fokus_cube/models/stone_10_1_2_5cm/model.sdf"
