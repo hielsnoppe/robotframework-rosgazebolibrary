64ec6d0655cd2a4c8a9754fd87bfad8c3d84b748
Niels Hoppe <mail@nielshoppe.de>
2022-05-26 12:31:08 +0200

    Add keywords; Add basic model management

--- src/RosGazeboLibrary/Gazebo.py
+++ src/RosGazeboLibrary/Gazebo.py
@@ -15,6 +15,7 @@ class Gazebo(object):
 
     def __init__(self):
         self.ros_lib = BuiltIn().get_library_instance('RosGazeboLibrary.ROS')
+        self.spawned_models = {}
 
     # Create and destroy models in simulation
     # http://gazebosim.org/tutorials/?tut=ros_comm#Services:Createanddestroymodelsinsimulation
@@ -22,6 +23,12 @@ class Gazebo(object):
     @keyword
     def spawn_urdf_model(self, urdf_path: str, position: tuple, model_name: str):
         ''' TODO: Refactor to use service call '''
+
+        self.spawned_models[model_name] = {
+            'type': 'URDF',
+            'path': urdf_path,
+            'position': position
+        }
 
         return self.ros_lib.rosrun('gazebo_ros', 'spawn_model', *[
             '-file', urdf_path,
@@ -35,6 +42,12 @@ class Gazebo(object):
     @keyword
     def spawn_sdf_model(self, sdf_path: str, position: tuple, model_name: str):
         ''' TODO: Refactor to use service call '''
+
+        self.spawned_models[model_name] = {
+            'type': 'SDF',
+            'path': sdf_path,
+            'position': position
+        }
 
         return self.ros_lib.rosrun('gazebo_ros', 'spawn_model', *[
             '-file', sdf_path,
@@ -51,10 +64,19 @@ class Gazebo(object):
         http://gazebosim.org/tutorials/?tut=ros_comm#DeleteModel
         '''
 
-        return self.ros_lib.call_service(
+        self.spawned_models.pop(model_name)
+
+        return self.ros_lib.rosservice_call(
             'gazebo/delete_model', 'gazebo_msgs/DeleteModel',
             { 'model_name': model_name }
             )
+
+    @keyword
+    def delete_spawned_models(self):
+        ''' Delete all previously spawned models from simulation
+        '''
+        for model_name in list(self.spawned_models.keys()):
+            self.delete_model(model_name)
 
     # State and property setters
     # http://gazebosim.org/tutorials/?tut=ros_comm#Services:Stateandpropertysetters
@@ -73,25 +95,41 @@ class Gazebo(object):
 
     @keyword
     def get_model_properties(self, model_name: str):
-        return self.ros_lib.call_service(
+        '''This service returns the properties of a model in simulation.'''
+        return self.ros_lib.rosservice_call(
             'gazebo/get_model_properties', 'gazebo_msgs/GetModelProperties',
             { 'model_name': model_name }
             )
 
     @keyword
     def get_model_state(self, model_name: str):
-        return self.ros_lib.call_service(
+        '''This service returns the states of a model in simulation.'''
+        return self.ros_lib.rosservice_call(
             'gazebo/get_model_state', 'gazebo_msgs/GetModelState',
             { 'model_name': model_name }
             )
+
+    @keyword
+    def get_world_properties(self):
+        '''This service returns the properties of the simulation world.'''
+        return self.ros_lib.rosservice_call(
+            'gazebo/get_model_state', 'gazebo_msgs/GetWorldProperties'
+            )
     
     ''' TODO
-    def get_world_properties(self, ...):
     def get_joint_properties(self, ...):
     def get_link_properties(self, ...):
     def get_link_state(self, ...):
-    def get_physics_properties(self, ...):
+    '''
 
+    @keyword
+    def get_physics_properties(self):
+        '''This service returns the properties of the physics engine used in simulation..'''
+        return self.ros_lib.rosservice_call(
+            'gazebo/get_physics_properties', 'gazebo_msgs/GetPhysicsProperties'
+            )
+
+    ''' TODO
     def link_states(self, ...): # investigate
     def model_states(self, ...): # investigate
     '''
@@ -138,6 +176,7 @@ class Gazebo(object):
     /gazebo_gui/get_loggers
     /gazebo_gui/set_logger_level
     '''
+    
     # Convenience keywords
 
     @keyword
